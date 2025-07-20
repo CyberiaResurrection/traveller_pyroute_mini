71ff727787136e8c0625c92d376b7a99d56fa381
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-07-20 18:06:55 +1000

    Add SystemStar and basic test

---
+++ Tests/SystemData/testSystemStar.py
@@ -0,0 +1,15 @@
+import unittest
+
+from SystemData.SystemStar import SystemStar
+
+
+class testSystemStar(unittest.TestCase):
+    def test_is_bigger(self):
+        star1 = SystemStar('V')
+        star2 = SystemStar('VI')
+
+        self.assertTrue(star1.is_bigger(star2))
+
+
+if __name__ == '__main__':
+    unittest.main()
