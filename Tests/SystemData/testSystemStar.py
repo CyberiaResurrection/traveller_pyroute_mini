4566dd002bbf3437262304c7f6452343092502e6
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-08-02 22:23:21 +1000

    Expand test to check star size

--- Tests/SystemData/testSystemStar.py
+++ Tests/SystemData/testSystemStar.py
@@ -5,10 +5,17 @@ from PyRoute.SystemData.SystemStar import SystemStar
 
 class testSystemStar(unittest.TestCase):
     def test_is_bigger(self):
-        star1 = SystemStar('V')
-        star2 = SystemStar('VI')
+        star1 = SystemStar('V', 'G', 2)
+        self.assertEqual('V', star1.size)
+        self.assertEqual('G', star1.spectral)
+        self.assertEqual(2, star1.digit)
+        star2 = SystemStar('VII', 'K', 7)
+        self.assertEqual('D', star2.size)
+        self.assertEqual('K', star2.spectral)
+        self.assertEqual(7, star2.digit)
 
         self.assertTrue(star1.is_bigger(star2))
+        self.assertFalse(star2.is_bigger(star1))
 
 
 if __name__ == '__main__':
